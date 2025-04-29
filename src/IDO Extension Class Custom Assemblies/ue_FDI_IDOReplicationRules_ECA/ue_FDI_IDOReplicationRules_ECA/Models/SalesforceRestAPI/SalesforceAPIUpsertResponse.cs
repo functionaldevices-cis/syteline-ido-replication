8f3f1acaed9808e2e7403ac19d8ef977b002cb90
Andy Mercer <61432947+amercer-fdi@users.noreply.github.com>
2025-04-29 13:16:12 -0400

    Add Upsert Response

---
+++ src/IDO Extension Class Custom Assemblies/ue_FDI_IDOReplicationRules_ECA/ue_FDI_IDOReplicationRules_ECA/Models/SalesforceRestAPI/SalesforceAPIUpsertResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ue_FDI_IDOReplicationRules_ECA.Models.SalesforceRestAPI
+{
+    public class SalesforceAPIUpsertResponse
+    {
+
+        public bool success
+        {
+            get; set;
+        }
+
+        public string message
+        {
+            get; set;
+        }
+
+        public SalesforceAPIUpsertResponse(bool success, string message)
+        {
+            this.success = success;
+            this.message = message;
+        }
+    }
+
+}
